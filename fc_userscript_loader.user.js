b2551d56a27fb014c24e51e08fd78a4ff31c615f
hashira222 <37973693+hashira222@users.noreply.github.com>
2018-04-01 17:19:25 +0900

    ユーザースクリプトローダー日本語対応

--- fc_userscript_loader.user.js
+++ fc_userscript_loader.user.js
@@ -4,18 +4,18 @@
 // @description    Userscript to load Frozen Cookies written by Icehawk78, forked by Lordshinjo
 // @author         Lordshinjo
 // @homepage       https://github.com/Lordshinjo/FrozenCookies
-// @include        http://orteil.dashnet.org/cookieclicker/
-// @updateURL      https://rawgit.com/Lordshinjo/FrozenCookies/master/fc_userscript_loader.js
-// @downloadURL    https://rawgit.com/Lordshinjo/FrozenCookies/master/fc_userscript_loader.js
+// @include        http://natto0wtr.web.fc2.com/CookieClicker/
+// @updateURL      https://rawgit.com/hashira222/FrozenCookies/master/fc_userscript_loader.js
+// @downloadURL    https://rawgit.com/hashira222/FrozenCookies/master/fc_userscript_loader.js
 // @run-at         document-start
 // ==/UserScript==
 
-// Dev:       https://github.com/Lordshinjo/FrozenCookies/development/
-// Master:    https://github.com/Lordshinjo/FrozenCookies/master/
-// Github.io: http://lordshinjo.github.io/FrozenCookies/
+// Dev:       https://github.com/hashira222/FrozenCookies/development/
+// Master:    https://github.com/hashira222/FrozenCookies/master/
+// Github.io: http://hashira222.github.io/FrozenCookies/
 
 function LoadFrozenCookies() {
-    Game.LoadMod('https://rawgit.com/Chevron42/FrozenCookies/master/frozen_cookies.js');
+    Game.LoadMod('https://rawgit.com/hashira222/FrozenCookies/master/frozen_cookies.js');
 }
 
 window.addEventListener("load", LoadFrozenCookies, false);
